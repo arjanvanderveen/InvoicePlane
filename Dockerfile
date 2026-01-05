6f296ac3c1c786ef66fdb184a72d99f438774436
Arjan van der Veen <avdveen@palanthir.nl>
2026-01-05 15:25:48 +0100

    Add own docker image creation

---
+++ Dockerfile
@@ -0,0 +1,66 @@
+FROM alpine:latest
+LABEL org.opencontainers.image.authors="avdveen@palanthir.nl"
+
+ENV PHP_VERSION="84" \
+    IP_SOURCE="https://github.com/InvoicePlane/InvoicePlane/releases/download" \
+    IP_VERSION="v1.6.3" \
+    MYSQL_HOST="mariadb_10_4" \
+    MYSQL_USER="root" \
+    MYSQL_PASSWORD="my-secret-pw" \
+    MYSQL_DB="invoiceplane" \
+    MYSQL_PORT="3306" \
+    IP_URL="http://127.0.0.1" \
+    HOST_URL="127.0.0.1" \
+    DISABLE_SETUP="false"
+
+RUN apk update                             \
+    &&  apk add nginx php${PHP_VERSION} php${PHP_VERSION}-fpm php${PHP_VERSION}-session \
+    php${PHP_VERSION}-gd php${PHP_VERSION}-mbstring php${PHP_VERSION}-mysqli php${PHP_VERSION}-openssl \
+    php${PHP_VERSION}-xml php${PHP_VERSION}-dom php${PHP_VERSION}-intl php${PHP_VERSION}-bcmath php${PHP_VERSION}-iconv \
+    php${PHP_VERSION}-simplexml php${PHP_VERSION}-tokenizer php${PHP_VERSION}-xmlwriter php${PHP_VERSION}-phar \
+    php${PHP_VERSION}-curl composer curl vim yarn git \
+    && mkdir -p /var/www/html/ \
+    && mkdir -p /run/nginx; \
+    [ -f /usr/bin/php ] && rm -f /usr/bin/php; \
+    ln -s /usr/bin/php${PHP_VERSION} /usr/bin/php;
+
+COPY . /var/www/html/
+
+# copy invoiceplane sources to web dir
+RUN cd /var/www/html && \
+    chmod +x /var/www/html/docker/start.sh; \
+    cp /var/www/html/docker/php.ini /etc/php${PHP_VERSION}/php.ini && \
+    cp /var/www/html/docker/php_fpm_site.conf /etc/php${PHP_VERSION}/php-fpm.d/www.conf && \
+    cp /var/www/html/docker/nginx_site.conf /etc/nginx/http.d/default.conf && \
+    /usr/bin/php${PHP_VERSION} /usr/bin/composer.phar update && \
+    yarn install && \
+    yarn build
+
+# add the themes available
+RUN cd /tmp && \
+    git clone https://github.com/InvoicePlane/InvoicePlane-Themes && \
+    cp -r /tmp/InvoicePlane-Themes/v1/* /var/www/html/assets/ && \
+    rm -rf /var/www/html/assets/README.md /var/www/html/assets/DEVELOPMENT.md
+
+# add the translations available
+ADD ${IP_SOURCE}/${IP_VERSION}/${IP_VERSION}.zip /tmp/
+
+RUN cd /tmp && \
+    unzip /tmp/${IP_VERSION}.zip && \
+    cp -r ip/application/language/* /var/www/html/application/language/
+
+# set the user rights straight and clean up
+RUN chown -R nobody:nginx /var/www/html/* && \
+    rm -rf /var/cache/apk/* \
+    rm -rf /tmp/*
+
+WORKDIR /var/www/html
+
+VOLUME /var/www/html/uploads /var/www/html/assets/core/css /var/www/html/application/views
+EXPOSE 80
+ENTRYPOINT ["/var/www/html/docker/start.sh"]
+CMD ["nginx", "-g", "daemon off;"]
+
+## Health Check
+HEALTHCHECK --interval=1m --timeout=3s --start-period=5s \
+  CMD curl -f http://127.0.0.1/index.php || exit 1
---
+++ docker/start.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+ln -s /dev/stdout /var/log/php${PHP_VERSION}/error.log
+ln -s /dev/stdout /var/log/nginx/access.log
+ln -s /dev/stdout /var/log/nginx/error.log
+
+
+if [ ! -f "/var/www/html/ipconfig.php" ]
+then
+  cp /var/www/html/ipconfig.php.example /var/www/html/ipconfig.php
+fi
+
+[ -n "$IP_URL" ] && sed -i -e "s!IP_URL=\$!IP_URL=${IP_URL}!" /var/www/html/ipconfig.php
+[ -n "$ENABLE_DEBUG" ] && sed -i -e "s/ENABLE_DEBUG=.*/ENABLE_DEBUG=${ENABLE_DEBUG}/" /var/www/html/ipconfig.php
+[ -n "$DISABLE_SETUP" ] && sed -i -e "s/DISABLE_SETUP=.*/DISABLE_SETUP=${DISABLE_SETUP}/" /var/www/html/ipconfig.php
+[ -n "$REMOVE_INDEXPHP" ] && sed -i -e "s/REMOVE_INDEXPHP=.*/REMOVE_INDEXPHP=${REMOVE_INDEXPHP}/" /var/www/html/ipconfig.php
+
+[ -n "$MYSQL_HOST" ] && sed -i -e "s/^DB_HOSTNAME=$/DB_HOSTNAME=${MYSQL_HOST}/" /var/www/html/ipconfig.php
+[ -n "$MYSQL_USER" ] && sed -i -e "s/^DB_USERNAME=$/DB_USERNAME=${MYSQL_USER}/" /var/www/html/ipconfig.php
+[ -n "$MYSQL_PASSWORD" ] && sed -i -e "s/^DB_PASSWORD=$/DB_PASSWORD=${MYSQL_PASSWORD}/" /var/www/html/ipconfig.php
+[ -n "$MYSQL_DB" ] && sed -i -e "s/^DB_DATABASE=$/DB_DATABASE=${MYSQL_DB}/" /var/www/html/ipconfig.php
+[ -n "$MYSQL_PORT" ] && sed -i -e "s/^DB_PORT=$/DB_PORT=${MYSQL_PORT}/" /var/www/html/ipconfig.php
+
+[ -n "$SESS_EXPIRATION" ] && sed -i -e "s/^SESS_EXPIRATION=$/SESS_EXPIRATION=${SESS_EXPIRATION}/" /var/www/html/ipconfig.php
+[ -n "$SESS_MATCH_IP" ] && sed -i -e "s/^SESS_MATCH_IP=$/SESS_MATCH_IP=${SESS_MATCH_IP}/" /var/www/html/ipconfig.php
+[ -n "$ENABLE_INVOICE_DELETION" ] && sed -i -e "s/^ENABLE_INVOICE_DELETION=$/ENABLE_INVOICE_DELETION=${ENABLE_INVOICE_DELETION}/" /var/www/html/ipconfig.php
+[ -n "$DISABLE_READ_ONLY" ] && sed -i -e "s/^DISABLE_READ_ONLY=$/DISABLE_READ_ONLY=${DISABLE_READ_ONLY}/" /var/www/html/ipconfig.php
+
+if [ $(grep 'PROXY_IPS' /var/www/html/ipconfig.php) -eq 1 ]
+then
+  [ -n "$PROXY_IPS" ] && sed -i -e "s!PROXY_IPS=\$!PROXY_IPS=${PROXY_IPS}!" /var/www/html/ipconfig.php
+else
+  echo "PROXY_IPS=${PROXY_IPS}" >> /var/www/html/ipconfig.php
+fi
+
+# set the config option SESS_MATCH_IP to False since it's giving problem accessing invoiceplane using ipv6
+sed -i 's/SESS_MATCH_IP=true/SESS_MATCH_IP=false/' /var/www/html/ipconfig.php
+
+# fix GLOB_BRACE bugs in index.php https://github.com/InvoicePlane/InvoicePlane/issues/1304
+sed -i 's/array_map(\x27unlink\x27, glob(UPLOADS_TEMP_FOLDER \. \x27\*\.{pdf,xml}\x27, \\GLOB_BRACE));/\$files = array_merge(\n    glob(UPLOADS_TEMP_FOLDER \. "\*\.pdf"),\n    glob(UPLOADS_TEMP_FOLDER \. "\*\.xml")\n);\narray_map("unlink", \$files);/' /var/www/html/index.php
+
+### CRITICAL
+if [ -n "$SETUP_COMPLETED" ]; then
+    [ -n "$ENCRYPTION_KEY" ] && sed -i -e "s/ENCRYPTION_KEY=.*/ENCRYPTION_KEY=${ENCRYPTION_KEY}/" /var/www/html/ipconfig.php;
+    [ -n "$ENCRYPTION_CIPHER" ] && sed -i -e "s/ENCRYPTION_CIPHER=.*/ENCRYPTION_CIPHER=${ENCRYPTION_CIPHER}/" /var/www/html/ipconfig.php;
+    sed -i -e "s/SETUP_COMPLETED=.*/SETUP_COMPLETED=${SETUP_COMPLETED}/" /var/www/html/ipconfig.php;
+fi
+
+[[ ! -d "/var/www/html/application/logs/" ]] && mkdir -p "/var/www/html/application/logs/"
+
+### Setup custom uploads
+if [ ! -f "/var/www/html/uploads/index.html" ]
+then
+  cp -r /tmp/ip/uploads/* /var/www/html/uploads/
+fi
+
+### Setup custom CSS
+if [ ! -f "/var/www/html/assets/core/css/custom.css" ]
+then
+  cp -r /tmp/ip/assets/core/css/* /var/www/html/assets/core/css/
+fi
+
+### Setup custom views
+if [ ! -f "/var/www/html/application/views/index.html" ]
+then
+  cp -r /tmp/ip/application/views/* /var/www/html/application/views/
+fi
+
+chown nobody:nginx /var/www/html/ipconfig.php;
+chown -R nobody:nginx /var/www/html/uploads;
+chown -R nobody:nginx /var/www/html/assets/core/css;
+chown -R nobody:nginx /var/www/html/application/views;
+
+php-fpm${PHP_VERSION}
+
+exec "$@"
